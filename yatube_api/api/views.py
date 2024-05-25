3cccba504bf76f1aee682ac99d5b5fc9ded0cb97
Андрей Андреев <darck2014@mail.ru>
2024-05-25 14:19:32 +0400

    2 review changes

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, mixins, status
+from rest_framework import viewsets, mixins
 from rest_framework.permissions import (
     IsAuthenticated,
     AllowAny,
